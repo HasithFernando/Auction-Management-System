b3b130a922e6f34571e2de91ed8335cba23b1f71
Sadeesha_Jayaweea <132116738+SadeeshaJayaweera@users.noreply.github.com>
2024-10-21 21:10:03 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -14,32 +14,34 @@ import AdminDashboard from './components/AdminDashboard/AdminDashboard';
 import UserManagement from './components/AdminDashboard/UserManagement';
 import AuctionManagement from './components/AdminDashboard/AuctionManagement';
 import BidManagement from './components/AdminDashboard/BidManagement';
-
+import PaymentPage from './pages/PaymentPage'; // Import PaymentPage
 
 const App = () => {
   return (
     <div className="d-flex flex-column min-vh-100">
       {/* Navbar */}
       <Navbar />
-      
+
       {/* Main Content */}
       <main className="flex-grow-1">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/LoginPage" element={<LoginPage />} />
           <Route path="/SignUp" element={<SignUp />} />
-          <Route path="/Product" element={<Product/>} />
-          <Route path="/profile" element={<ProfilePage/>} />
-          <Route path="/Auctions" element={<AuctionPage/>} />
-          <Route path="/AddAuction" element={<AddAuction/>} />
-          <Route path="/AdminDashboard" element={<AdminDashboard/>} />
-          <Route path="/AdminUsers" element={<UserManagement/>} />
-          <Route path="//AdminAuctions" element={<AuctionManagement/>} />
-          <Route path="//AdminBids" element={<BidManagement/>} />
+          <Route path="/Product" element={<Product />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/Auctions" element={<AuctionPage />} />
+          <Route path="/AddAuction" element={<AddAuction />} />
+          <Route path="/AdminDashboard" element={<AdminDashboard />} />
+          <Route path="/AdminUsers" element={<UserManagement />} />
+          <Route path="/AdminAuctions" element={<AuctionManagement />} />
+          <Route path="/AdminBids" element={<BidManagement />} />
           <Route path="/auctions/:id/bid" element={<BidInterface />} />
+          <Route path="/payment" element={<PaymentPage />} /> {/* Payment Route */}
         </Routes>
       </main>
 
+      {/* Footer */}
       <Footer />
     </div>
   );
